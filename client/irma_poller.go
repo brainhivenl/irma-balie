b1a9fb3a94ff9d156f2beb5416068db6b3e2eda9
Wouter Geraedts <git@woutergeraedts.nl>
2020-09-15 17:34:30 +0200

    Eliminated a superfluous select

--- client/irma_poller.go
+++ client/irma_poller.go
@@ -13,44 +13,41 @@ import (
 func irmaPollerDaemon(app *App) {
 	ticker := time.NewTicker(time.Second)
 
-	for {
-		select {
-		case <-ticker.C:
-			if app.State.SessionJwt == nil {
-				continue
-			}
+	for range ticker.C {
+		if app.State.SessionJwt == nil {
+			continue
+		}
 
-			sessionJwt := *app.State.SessionJwt
+		sessionJwt := *app.State.SessionJwt
 
-			resp, err := http.Post(fmt.Sprintf("%s/status", app.Cfg.ServerAddress), "application/jsonwebtoken", bytes.NewBuffer([]byte(sessionJwt)))
-			if err != nil && resp.StatusCode != 200 {
-				continue
-			}
+		resp, err := http.Post(fmt.Sprintf("%s/status", app.Cfg.ServerAddress), "application/jsonwebtoken", bytes.NewBuffer([]byte(sessionJwt)))
+		if err != nil && resp.StatusCode != 200 {
+			continue
+		}
 
-			bodyBytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal(err)
-				continue
-			}
-			status := string(bodyBytes)
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		status := string(bodyBytes)
 
-			messageValue := map[string]string{
-				"status": status,
-			}
+		messageValue := map[string]string{
+			"status": status,
+		}
 
-			messageValueMarshalled, err := json.Marshal(messageValue)
-			if err != nil {
-				continue
-			}
+		messageValueMarshalled, err := json.Marshal(messageValue)
+		if err != nil {
+			continue
+		}
 
-			app.Broadcaster.Notify(Message{
-				Type:  IrmaInProgress,
-				Value: messageValueMarshalled,
-			})
+		app.Broadcaster.Notify(Message{
+			Type:  IrmaInProgress,
+			Value: messageValueMarshalled,
+		})
 
-			if irmaStatusIsFinal(status) {
-				app.State = State{}
-			}
+		if irmaStatusIsFinal(status) {
+			app.State = State{}
 		}
 	}
 }
