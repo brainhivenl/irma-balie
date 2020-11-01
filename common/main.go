49c050b042f4b60a7e4ffd1ca25f2caec6e60dfb
Wouter Geraedts <git@woutergeraedts.nl>
2020-11-01 12:42:00 +0100

    Increased compute timeout

--- common/main.go
+++ common/main.go
@@ -92,7 +92,7 @@ func UnpackMrtd(mrtdCmd string, request MrtdRequest) (string, error) {
 		return "", err
 	}
 
-	return runMrtd(10*time.Second, mrtdCmd, requestBytes, false)
+	return runMrtd(30*time.Second, mrtdCmd, requestBytes, false)
 }
 
 // TestMrtd runs the external mrtd-unpack utility, without any input, to verify the functionality.
